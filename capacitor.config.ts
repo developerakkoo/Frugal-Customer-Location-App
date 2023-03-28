9207896b7a4db794480a07214b3a52349501a02a
Akshay Jadhav <akkooakshay@Apples-MacBook-Air.local>
2023-03-28 20:12:50 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frugal-location-web',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
